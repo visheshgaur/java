bacae5c99e0a7c1210b6e953f5ae55ae94f19e1e
vishesh gaur <gaurvishesh999@gmail.com>
2024-10-16 19:44:31 +0530

    second cmit

--- index.java
+++ index.java
@@ -1,5 +1,11 @@
 class index{
+    public static void printer(int arr[]){
+for(int i=0;i<arr.length;i++){
+    System.out.println(arr[i]);
+}
+    }
     public static void main(String args[]){
+        int arr[]={1,2,3,4};
         System.out.println("hello");
     }
 }
